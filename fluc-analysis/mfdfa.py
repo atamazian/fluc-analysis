c91c28dfee8c5f99f92d5a5d588f70c83c6c290a
Araik Tamazian <araik.tamazian@outlook.com>
2019-05-27 00:30:49 +0300

    Code optimization
    Optimized code to run more fast + moved Hurst exponent estimation to separate function

--- fluc-analysis/mfdfa.py
+++ fluc-analysis/mfdfa.py
@@ -3,43 +3,56 @@
 
 # Copyright (C) 2019 by Araik Tamazian
 
-import pandas as pd
 import numpy as np
-import math
 
 '''
-    This function is used for estimating Hurst parameter of time series based on J.W. Kantelhardt's paper (2002)
+    This function is used for fluctuation function estimation of the time series based on J.W. Kantelhardt's paper (2002)
         "Multifractal of detrended fluctuation analysis of non stationary time series"
-    Input parameters:   1, indata: Time series
+    Input parameters:   1, y: Time series
                         2, scale:  List or array variable define the sample sizes of the non-overlapping segments
                                 for instance: scale = np.logspace(np.log10(10**1),np.log10(10**5),30)
                         3, q: order statistical moment
                         4, m: Polynomial trend (m=1: linear, m=2: quadratic, m=3: cubic)
                         By default, q=2, m=1
-    Output:             1, H: Hurst parameters. It is a float variable
-                        2, scale: similar to input scale. It is a list (or array) variable
-                        3, F: The fluctuation function. It is a list variable
+    Output:             1, scale: similar to input scale. It is a list (or array) variable
+                        2, F: The fluctuation function. It is a list variable
 '''
 
-def mfdfa(x, scale, q=2, m=1):
-    y = np.cumsum(x-np.mean(x))                           #Equation 1 in paper
-    RMSt = []                                       #Temporary RMS variable: contain F(s,v) value
-    F = []                                          #F: Fluctuation function
-    for i in range(len(scale)):
-        ns = int(np.floor(len(y)/scale[i]))         #number of segments: Ns = int(N/s)
-        for v in range(ns):
-            index_start = v*scale[i]
-            index_end = (v+1)*scale[i]
-            index = range(index_start,index_end)    #calculate index for each segment
-            yv = y[index_start:index_end]           #Extract values of time series for each segments
-            c = np.polyfit(index,yv,m)
-            fit = np.polyval(c,index)
-            RMSt.append(math.sqrt(np.mean((yv-fit)**2))) #Equation 2. But calculating only F(v,s) not F(v,s)**2
-        RMS = np.asarray(RMSt)                      #Convert RMSt to array
-        qRMS = RMS**q
-        F.append(np.mean(qRMS)**(1.0/q))              #Equation 4
-        del RMSt[:]                                 #Reset RMSt[:]
-    C = np.polyfit(np.log10(scale),np.log10(F),m)
-    H = C[0]                                        #Hurst parameter
-    return (H,scale,F)
+def mfdfa(y, scale, q=2, m=1):
+    n = y.shape[0]
+    nscale = scale.shape[0]
+    y = np.cumsum(y-np.mean(y))                           
+    
+    def seg_calc(i):
+        ns = int(np.floor(n/scale[i]))         #number of segments: Ns = int(N/s)
+		
+        ind = np.arange(ns*scale[i])
+        index = np.split(ind, ns)
+        yv = np.split(y[ind], ns)
+        RMSt = np.empty(ns)
+        
+        def seg_fit(v):
+            c = np.polyfit(index[v],yv[v],m)
+            fit = np.polyval(c,index[v])
+            return np.sqrt(np.mean(np.power(yv[v]-fit, 2)))            
+
+        vfunc = np.vectorize(seg_fit)
+        RMSt = vfunc(range(ns))     
+        qRMS = np.power(RMSt, q)
+        return np.power(np.mean(qRMS), 1.0/q)
+
+    vfunc2 = np.vectorize(seg_calc)
+    F = vfunc2(range(nscale))
+    return (scale,F)
+ 
+'''
+    This function is used for estimation of Hust exponent
+    Input parameters:   1, scale:  List or array variable define the sample sizes of the non-overlapping segments
+                                for instance: scale = np.logspace(np.log10(10**1),np.log10(10**5),30)
+                        2, F: Flunctuation function values array
+    Output:             1, Hurst exponent
+'''
+ 
+ def getHurst(scale, F):
+    return np.polyfit(np.log10(scale),np.log10(F), 1)[0]
 
