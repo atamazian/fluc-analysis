eee4731d2261e354eb0c45b79270e1c1bd411b00
Araik Tamazian <araik.tamazian@outlook.com>
2019-05-27 13:59:31 +0300

    Minor fix

--- fluc-analysis/schreiber.py
+++ fluc-analysis/schreiber.py
@@ -11,7 +11,7 @@
 import numpy as np
 import scipy.special
 import cmath
-from mfdfa import mfdfa, getHurst
+from mfdfa import mfdfa, get_hurst, create_logscale
 from fgnoise import fgnoise
 
 def schreiber_schmitz(x, h, htol, maxitr):
@@ -20,13 +20,12 @@ def schreiber_schmitz(x, h, htol, maxitr):
     he = 0
     n = len(x)
     x = np.sort(x)
-    scale = np.logspace(np.log10(10**1),np.log10(n/4.0),10)
-    scale = scale.astype(int)
+    scale = create_logscale(1, n/4, 100)
     while((he < h-htol)|(he > h+htol))&(itr < maxitr):
         y = fgnoise(n, h1)
         p = sorted(range(len(y)),key=lambda x:y[x])
         y[p] = x
-        he = getHurst(scale, mfdfa(y, scale, 2, 1)[1])
+        he = get_hurst(scale, mfdfa(y, scale, 2, 1)[1])
         h1 = h1 + (h1 - he)
         itr = itr +1
     return y, he, itr
